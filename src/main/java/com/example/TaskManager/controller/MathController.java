85cf8a2fa0b0b2722a33d950b1ddd78b6ee395b2
Arisbek <71332684+Arisbek@users.noreply.github.com>
2024-12-11 13:35:02 +0600

    final

--- src/main/java/com/example/TaskManager/controller/MathController.java
+++ src/main/java/com/example/TaskManager/controller/MathController.java
@@ -4,7 +4,7 @@ import com.example.TaskManager.service.MathService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.*;
-import io.swagger.v3.oas.annotations.Operation;
+
 
 @RestController
 @RequestMapping("/math")
@@ -13,13 +13,11 @@ public class MathController {
     @Autowired
     private MathService mathService;
 
-    @Operation(summary = "Get a greeting message")
     @GetMapping("/add")
     public double add(@RequestParam double a, @RequestParam double b) {
         return mathService.add(a, b);
     }
 
-    @Operation(summary = "Get a personalized greeting")
     @GetMapping("/subtract")
     public double subtract(@RequestParam double a, @RequestParam double b) {
         return mathService.subtract(a, b);
@@ -33,5 +31,4 @@ public class MathController {
     @GetMapping("/divide")
     public double divide(@RequestParam double a, @RequestParam double b) {
         return mathService.divide(a, b);
-    }
-}
+    }}
